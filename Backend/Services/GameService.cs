1e61df79af8893360522d521f10eec4407e27b3d
Isaac Arcen <isaac.arcen@edu.newton.se>
2026-03-26 12:24:19 +0100

    GameService Game.cs simpler Player.cs simpler GameService : if statements waiting for methods

---
+++ Backend/Services/GameService.cs
@@ -0,0 +1,65 @@
+﻿using LetterDuel.Backend.Domain;
+
+namespace LetterDuel.Backend.Services
+{
+    public class GameService
+    {
+        public Game CreateGame(string secretWord)
+        {
+            return new Game
+            {
+                SecretWord = secretWord.ToUpperInvariant(),
+                State = GameState.WaitingForPlayers,
+                CurrentPlayerIndex = 0
+            };
+        }
+
+        public void AddPlayer(Game game, Player player)
+        {
+            if (game.Players.Count >= 2)
+            {
+                throw new InvalidOperationException("Game already has two players.");
+            }
+
+            player.GameId = game.Id;
+            game.Players.Add(player);
+
+            if (game.Players.Count == 2)
+            {
+                game.State = GameState.InProgress;
+            }
+        }
+
+        public void GuessLetter(Game game, Guid playerId, char letter)
+        {
+            if (game.State != GameState.InProgress)
+            {
+                throw new InvalidOperationException("Game is not in progress");
+            }
+
+            if (game.Players[game.CurrentPlayerIndex].Id != playerId)
+            {
+                throw new InvalidOperationException("It is not this players turn");
+            }
+            letter = char.ToUpperInvariant(letter);
+
+            if (game.GuessedLetters.Contains(letter))
+            {
+                throw new InvalidOperationException("Letter has already been guessed.");
+            }
+            game.GuessedLetters.Add(letter);
+
+            if (game.SecretWord.Contains(letter))
+            {
+                var currentPlayer = game.Players[game.CurrentPlayerIndex];
+                currentPlayer.Score += IsVowel(letter) ? 2 : 4;
+            }
+
+            if (IsWordFullyGuessed(game))
+            {
+                game.State = GameState.GameFinished;
+                return;
+            }
+        }
+    }
+}
