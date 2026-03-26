6255f3c2286577a85cdcc6558f7dc19927a5c7a9
AngelosLekkas <lekkas.angelos@gmail.com>
2026-03-26 12:57:56 +0100

    GameWord model added.

---
+++ Backend/Domain/GameWord.cs
@@ -0,0 +1,10 @@
+﻿namespace LetterDuel.Backend.Domain
+{
+    public class GameWord
+    {
+        public int Id { get; set; }
+        public string Word { get; set; } = string.Empty;
+    }
+}
+
+//vi seedar en GameWord lista i vår db som service kan slumpa ett ord ifrån.
