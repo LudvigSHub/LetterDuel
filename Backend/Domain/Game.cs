6b9e530db0f9da0f57151224ef7ab2f3881637e3
Isaac Arcen <isaac.arcen@edu.newton.se>
2026-03-26 09:41:55 +0100

    e

---
+++ Backend/Domain/Game.cs
@@ -0,0 +1,19 @@
+﻿namespace LetterDuel.Backend.Domain
+{
+    public class Game
+    {
+        public Guid Id { get; init; } = Guid.NewGuid();
+        public string SecretWord { get; }
+        public GameState State { get; private set; }
+        public List<Player> Players { get; } = new();
+        public HashSet<char> GuessedLetters { get; } = new();
+
+        public Game(string secretWord)
+        {
+            SecretWord = secretWord.ToUpperInvariant();
+            State = GameState.WaitingForPlayers;
+        }
+
+        public int WordLength => SecretWord.Length;
+    }
+}
---
+++ Backend/Domain/Player.cs
@@ -0,0 +1,9 @@
+﻿namespace LetterDuel.Backend.Domain
+{
+    public class Player
+    {
+        public Guid Id { get; init; } = Guid.NewGuid();
+        public string Name { get; set; }
+        public int Score { get; set; }
+    }
+}
