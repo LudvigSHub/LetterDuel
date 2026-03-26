d42ffa0b7b4f068576ba636a07fbb51dec62d01f
Isaac Arcen <isaac.arcen@edu.newton.se>
2026-03-26 11:23:12 +0100

    hue

--- LetterDuel.Tests/GameTests.cs
+++ LetterDuel.Tests/GameTests.cs
@@ -16,5 +16,37 @@ namespace LetterDuel.Tests
             Assert.Equal(GameState.WaitingForPlayers, game.State);
 
         }
+
+        [Fact]
+        public void NewGame_WordInUpperCase()
+        {
+            var game = new Game("apple");
+
+            Assert.Equal("APPLE", game.SecretWord);
+        }
+
+        [Fact] 
+        public void NewGame_WordLength()
+        {
+            var game = new Game("apple");
+
+            Assert.Equal(5, game.WordLength);
+        }
+
+        [Fact]
+        public void NewGame_StartWithNoPlayers()
+        {
+            var game = new Game("apple");
+
+            Assert.Empty(game.Players);
+        }
+
+        [Fact]
+        public void NewGame_StartWithNoGuessedLetters()
+        {
+            var game = new Game("apple");
+
+            Assert.Empty(game.GuessedLetters);
+        }
     }
 }
