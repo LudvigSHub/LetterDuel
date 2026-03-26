061962f3105fcfec7b4f0051e137b88eeb313342
Isaac Arcen <isaac.arcen@edu.newton.se>
2026-03-26 09:47:09 +0100

    comments

--- Backend/Domain/Game.cs
+++ Backend/Domain/Game.cs
@@ -2,10 +2,13 @@
 {
     public class Game
     {
+
         public Guid Id { get; init; } = Guid.NewGuid();
+        
         public string SecretWord { get; }
         public GameState State { get; private set; }
         public List<Player> Players { get; } = new();
+        //sparar bokstäver som redan gissats
         public HashSet<char> GuessedLetters { get; } = new();
 
         public Game(string secretWord)
@@ -14,6 +17,7 @@
             State = GameState.WaitingForPlayers;
         }
 
+        //hjälp-egenskap för att kunna visa ordets längd i UI
         public int WordLength => SecretWord.Length;
     }
 }
