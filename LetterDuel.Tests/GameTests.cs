84036b4b506fa4d041a86782b7af7cb8d6fb3e15
Isaac Arcen <isaac.arcen@edu.newton.se>
2026-03-26 10:11:52 +0100

    xunit created, PlayerTests GameTests

---
+++ LetterDuel.Tests/GameTests.cs
@@ -0,0 +1,19 @@
+﻿using LetterDuel.Backend.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LetterDuel.Tests
+{
+    public class GameTests
+    {
+        [Fact]
+        public void NewGameWaitingForPlayers()
+        {
+            var game = new Game("apple");
+
+            Assert.Equal(GameState.WaitingForPlayers, game.State);
+
+        }
+    }
+}
---
+++ LetterDuel.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace LetterDuel.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
