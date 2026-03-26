ae76dab73c5c1240a843a519d25f073928985994
Isaac Arcen <isaac.arcen@edu.newton.se>
2026-03-26 09:45:01 +0100

    comments

--- Backend/Domain/Player.cs
+++ Backend/Domain/Player.cs
@@ -2,8 +2,12 @@
 {
     public class Player
     {
+        // Guid används för att ge varje spelare ett unikt id direkt vid skapandet,
+        // utan att vara beroende av en databas eller en räknare som genererar löpnummer. 
         public Guid Id { get; init; } = Guid.NewGuid();
+        //spelarens namn
         public string Name { get; set; }
+        //spelarens nuvarande poäng under nuvarande spel
         public int Score { get; set; }
     }
 }
