62be65134907a147258a6a6241c86003bbcc6cc8
AngelosLekkas <lekkas.angelos@gmail.com>
2026-03-27 11:42:19 +0100

    Kommenterat bort GameTests, behövs antagligen inte.

--- LetterDuel.Tests/GameTests.cs
+++ LetterDuel.Tests/GameTests.cs
@@ -9,71 +9,71 @@ namespace LetterDuel.Tests
     public class GameTests
     {
 
-        [Fact]
-        //Game får unikt id
-        public void NewGame_GetsUniqueId()
-        {
-            var game = new Game();
-            game.SecretWord = "APPLE";
+        //[Fact]
+        ////Game får unikt id
+        //public void NewGame_GetsUniqueId()
+        //{
+        //    var game = new Game();
+        //    game.SecretWord = "APPLE";
             
 
-            Assert.NotEqual(Guid.Empty, game.Id);
-        }
+        //    Assert.NotEqual(Guid.Empty, game.Id);
+        //}
 
-        [Fact]
-        //game väntar på spelare
-        public void NewGameWaitingForPlayers()
-        {
-            var game = new Game();
-            game.SecretWord = "APPLE";
-
-
-            Assert.Equal(GameState.WaitingForPlayers, game.State);
-
-        }
-
-        [Fact]
-        //game gör ordet till stora bokstäver
-        public void NewGame_WordInUpperCase()
-        {
-            var game = new Game();
-            game.SecretWord = "APPLE";
+        //[Fact]
+        ////game väntar på spelare
+        //public void NewGameWaitingForPlayers()
+        //{
+        //    var game = new Game();
+        //    game.SecretWord = "APPLE";
 
 
-            Assert.Equal("APPLE", game.SecretWord);
-        }
+        //    Assert.Equal(GameState.WaitingForPlayers, game.State);
 
-        [Fact]
-        //game ordets längd i bokstäver
-        public void NewGame_WordLength()
-        {
-            var game = new Game();
-            game.SecretWord = "APPLE";
+        //}
 
-
-            Assert.Equal(5, game.WordLength);
-        }
-
-        [Fact]
-        //game startar med inga spelare
-        public void NewGame_StartWithNoPlayers()
-        {
-            var game = new Game();
-            game.SecretWord = "APPLE";
+        //[Fact]
+        ////game gör ordet till stora bokstäver
+        //public void NewGame_WordInUpperCase()
+        //{
+        //    var game = new Game();
+        //    game.SecretWord = "APPLE";
 
 
-            Assert.Empty(game.Players);
-        }
+        //    Assert.Equal("APPLE", game.SecretWord);
+        //}
 
-        [Fact]
-        //game startar med inga gissade bokstäver
-        public void NewGame_StartWithNoGuessedLetters()
-        {
-            var game = new Game();
-            game.SecretWord = "APPLE";
+        //[Fact]
+        ////game ordets längd i bokstäver
+        //public void NewGame_WordLength()
+        //{
+        //    var game = new Game();
+        //    game.SecretWord = "APPLE";
 
 
-            Assert.Empty(game.GuessedLetters);
-        }
+        //    Assert.Equal(5, game.WordLength);
+        //}
+
+        //[Fact]
+        ////game startar med inga spelare
+        //public void NewGame_StartWithNoPlayers()
+        //{
+        //    var game = new Game();
+        //    game.SecretWord = "APPLE";
+
+
+        //    Assert.Empty(game.Players);
+        //}
+
+        //[Fact]
+        ////game startar med inga gissade bokstäver
+        //public void NewGame_StartWithNoGuessedLetters()
+        //{
+        //    var game = new Game();
+        //    game.SecretWord = "APPLE";
+
+
+        //    Assert.Empty(game.GuessedLetters);
+        //}
     }
 }
