95d02cd0a987139772024647520416c3ba913f45
AngelosLekkas <lekkas.angelos@gmail.com>
2026-03-31 13:56:35 +0200

    IGameRepository & InMemoryGameRepository färdiga

--- Backend/Repositories/IGameRepository.cs
+++ Backend/Repositories/IGameRepository.cs
@@ -1,7 +1,13 @@
-﻿namespace LetterDuel.Backend.Repositories
+﻿using LetterDuel.Backend.Domain;
+
+namespace LetterDuel.Backend.Repositories
 {
     public interface IGameRepository
     {
+        //hämtar ett game baserat på id
+        Game? GetById(Guid id);
 
+        //spara new game eller updatera ett befintligt game
+        void Save(Game game);
     }
 }
