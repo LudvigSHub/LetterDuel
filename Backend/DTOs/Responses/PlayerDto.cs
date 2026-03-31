596618af81784c942433f3f5925949ed4ca97bf1
Isaac Arcen <isaac.arcen@edu.newton.se>
2026-03-31 12:31:10 +0200

    g

--- Backend/DTOs/Responses/PlayerDto.cs
+++ Backend/DTOs/Responses/PlayerDto.cs
@@ -3,7 +3,7 @@
     public class PlayerDto
     {
         public Guid Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
         public int Score { get; set; }
     }
 }
