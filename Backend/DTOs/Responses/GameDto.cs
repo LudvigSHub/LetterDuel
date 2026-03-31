44759553e1b7b32691885eae974731d1dc1593b8
Isaac Arcen <isaac.arcen@edu.newton.se>
2026-03-31 12:29:06 +0200

    gameDto + GuessedLetters WinnerId

--- Backend/DTOs/Responses/GameDto.cs
+++ Backend/DTOs/Responses/GameDto.cs
@@ -10,6 +10,8 @@ namespace LetterDuel.Backend.DTOs.Responses
         public List<PlayerDto> Players { get; set; } = new();
         public string State { get; set; } = "";
         //id på spelarens vars tur det är
+        public List<char> GuessedLetters { get; set; } = new();
+        public Guid? WinnerId { get; set; }
         public Guid CurrentPlayerId { get; set; }
 
         public static GameDto FromGame(Game game)
@@ -34,7 +36,18 @@ namespace LetterDuel.Backend.DTOs.Responses
                 //hämtar id för spelarens tur det är
                 //UI använder detta för att avgöra vem som får spela
                 State = game.State.ToString(),
-                CurrentPlayerId = game.Players[game.CurrentPlayerIndex].Id
+                CurrentPlayerId = game.Players[game.CurrentPlayerIndex].Id,
+
+                GuessedLetters = game.GuessedLetters
+                    .OrderBy(l => l)
+                    .ToList(),
+
+               
+                WinnerId = game.State == GameState.GameFinished
+                    ? game.Players
+                    .OrderByDescending(p => p.Score)
+                    .FirstOrDefault()?.Id
+                    : null
             };
         }
     }
