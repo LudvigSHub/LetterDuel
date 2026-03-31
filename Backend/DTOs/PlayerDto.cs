e5358c747f9f15310a9c41ed4312d143d8213837
Isaac Arcen <isaac.arcen@edu.newton.se>
2026-03-31 12:10:42 +0200

    playerDto

---
+++ Backend/DTOs/PlayerDto.cs
@@ -0,0 +1,9 @@
+﻿namespace LetterDuel.Backend.DTOs
+{
+    public class PlayerDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Score { get; set; }
+    }
+}
