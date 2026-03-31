944c758072610d2f8d86b845353accb45741e1d8
Isaac Arcen <isaac.arcen@edu.newton.se>
2026-03-31 12:25:58 +0200

    comments GameDto

--- Backend/DTOs/Responses/GameDto.cs
+++ Backend/DTOs/Responses/GameDto.cs
@@ -4,28 +4,35 @@ namespace LetterDuel.Backend.DTOs.Responses
 {
     public class GameDto
     {
+        //spelets id
         public Guid Id { get; set; }
         public string MaskedWord { get; set; } = string.Empty;
         public List<PlayerDto> Players { get; set; } = new();
         public string State { get; set; } = "";
+        //id på spelarens vars tur det är
         public Guid CurrentPlayerId { get; set; }
 
         public static GameDto FromGame(Game game)
         {
             return new GameDto
             {
+                //Kopierar spelets id
                 Id = game.Id,
+                //skapar en maskerad version av ordet
                 MaskedWord = new string(
                     game.SecretWord.Select(letter =>
                         game.GuessedLetters.Contains(letter) ? letter : '_'
                     ).ToArray()
                 ),
+                //mappar varje player med playerdto
                 Players = game.Players.Select(p => new PlayerDto
                 {
                     Id = p.Id,
                     Name = p.Name,
                     Score = p.Score,
                 }).ToList(),
+                //hämtar id för spelarens tur det är
+                //UI använder detta för att avgöra vem som får spela
                 State = game.State.ToString(),
                 CurrentPlayerId = game.Players[game.CurrentPlayerIndex].Id
             };
