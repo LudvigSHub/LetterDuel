bc43d4eb570eb04963e15e505b105443e77036d4
Isaac Arcen <isaac.arcen@edu.newton.se>
2026-03-31 15:23:49 +0200

    testing updated

--- LetterDuel.Tests/GameServiceTests.cs
+++ LetterDuel.Tests/GameServiceTests.cs
@@ -1,76 +1,78 @@
 ﻿using LetterDuel.Backend.Domain;
 using LetterDuel.Backend.Services;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace LetterDuel.Tests
 {
     public class GameServiceTests
     {
-        private readonly GameService _service = new();
+        private readonly GameService _service;
+
+        public GameServiceTests()
+        {
+            var repo = new FakeGameRepository();
+            _service = new GameService(repo);
+        }
 
         [Fact]
         //new game ska ha WaitingForPlayers state 
         //creator ska vara player1 = [0]
-        public void CreateGame_SetPlayer1_as_Creator()
+        public async Task CreateGame_SetPlayer1_as_Creator()
         {
-            var creator = new Player { Name = "Player1" };
-            var game = _service.CreateGame("APPLE", creator);
+            var game = await _service.CreateGame("APPLE", "Player1");
 
             Assert.Single(game.Players);
-            Assert.Equal(creator.Id, game.Players[0].Id);
+            Assert.Equal("Player1", game.Players[0].Name);
             Assert.Equal(GameState.WaitingForPlayers, game.State);
-            Assert.Equal(game.Id, creator.GameId);
             Assert.Equal(0, game.CurrentPlayerIndex);
         }
 
         [Fact]
         //AddPlayer ska lägga till en spelare i spelet
-        public void AddPlayer_Should_Add_Player_To_Game()
+        public async Task AddPlayer_Should_Add_Player_To_Game()
         {
-            var creator = new Player { Name = "Player1" };
-            var player2 = new Player { Name = "Player2" };
-            var game = _service.CreateGame("APPLE", creator);
+            var game = await _service.CreateGame("APPLE", "Player1");
 
-            _service.AddPlayer(game, player2);
+            var updated = await _service.AddPlayer(game.Id, "Player2");
 
-            Assert.Equal(2, game.Players.Count); //antal spelare sätts till 2
-            Assert.Equal(player2.Id, game.Players[1].Id); //player2 id stämmer överens med play[1].Id
-            Assert.Equal(game.Id, player2.GameId); //player2 has same gameId as game
-            Assert.Equal(GameState.InProgress, game.State); //gamestate ska sättas till InProgress när player2 joinar
+            Assert.Equal(2, updated!.Players.Count);
+            Assert.Equal("Player2", updated.Players[1].Name);
+            Assert.Equal(GameState.InProgress, updated.State);
         }
 
         [Fact]
         //efter player2 har joinat ska inga fler spelare kunna ansluta
-        public void Max_2_players_allowed()
+        public async Task Max_2_players_allowed()
         {
-            var (game, player1, player2) = CreateStartedGame();
-            var player3 = new Player { Name = "Player3" };
-            var action = () => _service.AddPlayer(game, player3);
+            var game = await _service.CreateGame("APPLE", "P1");
+            await _service.AddPlayer(game.Id, "P2");
 
-            Assert.Throws<InvalidOperationException>(action);
+            var action = async () => await _service.AddPlayer(game.Id, "P3");
+
+            await Assert.ThrowsAsync<InvalidOperationException>(action);
         }
 
         [Fact]
         //Game måste vara InProgress för att man ska kunna gissa
-        public void Game_Has_To_Be_In_Progress_To_Guess()
+        public async Task Game_Has_To_Be_In_Progress_To_Guess()
         {
-            var creator = new Player { Name = "Player1" };
-            var game = _service.CreateGame("APPLE", creator);
-            var action = () => _service.GuessLetter(game, creator.Id, "A");
+            var game = await _service.CreateGame("APPLE", "P1");
 
-            Assert.Throws<InvalidOperationException>(action);
+            var action = async () =>
+                await _service.GuessLetter(game.Id, game.Players[0].Id, "A");
+
+            await Assert.ThrowsAsync<InvalidOperationException>(action);
         }
 
         [Fact]
         //Player2 ska inte kunna gissa när det är P1 tur
-        public void P2_Cant_Guess_While_P1_Turn()
+        public async Task P2_Cant_Guess_While_P1_Turn()
         {
-            var (game, player1, player2) = CreateStartedGame();
-            var action = () => _service.GuessLetter(game, player2.Id, "A");
+            var (game, player1, player2) = await CreateStartedGame();
 
-            Assert.Throws<InvalidOperationException>(action);
+            var action = async () =>
+                await _service.GuessLetter(game.Id, player2.Id, "A");
+
+            await Assert.ThrowsAsync<InvalidOperationException>(action);
         }
 
         [Theory]
@@ -82,12 +84,14 @@ namespace LetterDuel.Tests
         [InlineData("Ä")]
         [InlineData("Ö")]
         //restrictions, input får ej vara special chars
-        public void Guess_Restrictionz(string input)
+        public async Task Guess_Restrictionz(string input)
         {
-            var (game, player1, player2) = CreateStartedGame();
-            var action = () => _service.GuessLetter(game, player1.Id, input);
+            var (game, player1, _) = await CreateStartedGame();
 
-            Assert.Throws<InvalidOperationException>(action);
+            var action = async () =>
+                await _service.GuessLetter(game.Id, player1.Id, input);
+
+            await Assert.ThrowsAsync<InvalidOperationException>(action);
         }
 
         [Theory]
@@ -96,41 +100,41 @@ namespace LetterDuel.Tests
         [InlineData("APPLE", "Z", false, 0)]
         [InlineData("LETTER", "T", true, 8)]
         //vokaler ger 2p, kononanter ger 4p, fel 0p
-        public void GuessLetter_Should_Save_Letter_And_Update_Score_Correctly
-           (string secretWord,
+        public async Task GuessLetter_Should_Save_Letter_And_Update_Score_Correctly(
+            string secretWord,
             string input,
             bool shouldExistInWord,
             int expectedScore)
         {
-            var (game, player1, player2) = CreateStartedGame(secretWord);
-            _service.GuessLetter(game, player1.Id, input);
+            var (game, player1, _) = await CreateStartedGame(secretWord);
 
-            Assert.Contains(input.ToUpperInvariant()[0], game.GuessedLetters);
-            Assert.Equal(shouldExistInWord, game.SecretWord.Contains(input.ToUpperInvariant()[0]));
-            Assert.Equal(expectedScore, player1.Score);
+            var updated = await _service.GuessLetter(game.Id, player1.Id, input);
 
+            Assert.Contains(input.ToUpperInvariant()[0], updated!.GuessedLetters);
+            Assert.Equal(shouldExistInWord, updated.SecretWord.Contains(input.ToUpperInvariant()[0]));
+            Assert.Equal(expectedScore, updated.Players[0].Score);
         }
 
         [Fact]
-        public void SwitchTurns_After_Guess()
+        public async Task SwitchTurns_After_Guess()
         {
-            var (game, player1, player2) = CreateStartedGame();
-            _service.GuessLetter(game, player1.Id, "A");
+            var (game, player1, _) = await CreateStartedGame();
 
-            Assert.Equal(1, game.CurrentPlayerIndex);
+            var updated = await _service.GuessLetter(game.Id, player1.Id, "A");
+
+            Assert.Equal(1, updated!.CurrentPlayerIndex);
         }
 
         [Theory]
         [InlineData("WORD", "", "____")]
         [InlineData("WORD", "WO", "WO__")]
         [InlineData("WORD", "WORD", "WORD")]
-        public void GetMaskedWord_Should_Return_Correct_Masked_Word
-        (string secretWord,
-        string guessedLetters,
-        string expectedMaskedWord)
+        public async Task GetMaskedWord_Should_Return_Correct_Masked_Word(
+            string secretWord,
+            string guessedLetters,
+            string expectedMaskedWord)
         {
-            var creator = new Player { Name = "Player1" };
-            var game = _service.CreateGame(secretWord, creator);
+            var game = await _service.CreateGame(secretWord, "P1");
 
             game.GuessedLetters = guessedLetters;
 
@@ -139,59 +143,47 @@ namespace LetterDuel.Tests
             Assert.Equal(expectedMaskedWord, result);
         }
 
-
-
-
         //hjälpmetod för att skapa ett spel som är InProgress med 2 players
-        private (Game game, Player player1, Player player2) CreateStartedGame(string secretWord = "APPLE")
+        private async Task<(Game game, Player player1, Player player2)> CreateStartedGame(string secretWord = "APPLE")
         {
-            var player1 = new Player
-            {
-                Name = "Player 1"
-            };
+            var game = await _service.CreateGame(secretWord, "P1");
+            var updated = await _service.AddPlayer(game.Id, "P2");
 
-            var player2 = new Player
-            {
-                Name = "Player 2"
-            };
-
-            var game = _service.CreateGame(secretWord, player1);
-            _service.AddPlayer(game, player2);
-
-            return (game, player1, player2);
+            return (updated!, updated!.Players[0], updated.Players[1]);
         }
 
         [Fact]
         //Om bokstav redan gissats
-        public void If_Letter_Is_Guessed_Already()
+        public async Task If_Letter_Is_Guessed_Already()
         {
-            var (game, player1, _) = CreateStartedGame();
+            var (game, player1, _) = await CreateStartedGame();
 
-            _service.GuessLetter(game, player1.Id, "A");
+            await _service.GuessLetter(game.Id, player1.Id, "A");
 
-            var action = () => _service.GuessLetter(game, player1.Id, "A");
+            var action = async () =>
+                await _service.GuessLetter(game.Id, player1.Id, "A");
 
-            Assert.Throws<InvalidOperationException>(action);
+            await Assert.ThrowsAsync<InvalidOperationException>(action);
         }
 
         [Fact]
-        public void Game_Ending_Test()
+        public async Task Game_Ending_Test()
         {
-            var (game, player1, player2) = CreateStartedGame("A");
+            var (game, player1, _) = await CreateStartedGame("A");
 
-            _service.GuessLetter(game, player1.Id, "A");
+            var updated = await _service.GuessLetter(game.Id, player1.Id, "A");
 
-            Assert.Equal(GameState.GameFinished, game.State);
+            Assert.Equal(GameState.GameFinished, updated!.State);
         }
 
         [Fact]
         //GetWinner ska returnera den spelare som har högst poäng
-        public void GetWinner_Test()
+        public async Task GetWinner_Test()
         {
-            var (game, player1, player2) = CreateStartedGame("AB");
+            var (game, player1, player2) = await CreateStartedGame("AB");
 
-            _service.GuessLetter(game, player1.Id, "A");
-            _service.GuessLetter(game, player2.Id, "B");
+            await _service.GuessLetter(game.Id, player1.Id, "A");
+            await _service.GuessLetter(game.Id, player2.Id, "B");
 
             game.State = GameState.GameFinished;
 
@@ -202,12 +194,14 @@ namespace LetterDuel.Tests
 
         [Fact]
         //test att spelet tar emot stor bokstav oavsett
-        public void GuessLetter_Should_Be_Case_Insensitive()
+        public async Task GuessLetter_Should_Be_Case_Insensitive()
         {
-            var (game, player1, player2) = CreateStartedGame("APPLE");
-            _service.GuessLetter(game, player1.Id, "a");
-            Assert.Contains('A', game.GuessedLetters);
-            Assert.Equal(2, player1.Score);
+            var (game, player1, _) = await CreateStartedGame("APPLE");
+
+            var updated = await _service.GuessLetter(game.Id, player1.Id, "a");
+
+            Assert.Contains('A', updated!.GuessedLetters);
+            Assert.Equal(2, updated.Players[0].Score);
         }
     }
 }
