9ad36d256192220efa8640fd9b7191ac51bf8695
Isaac Arcen <isaac.arcen@edu.newton.se>
2026-03-31 11:42:06 +0200

    comments

--- Backend/Controllers/GameController.cs
+++ Backend/Controllers/GameController.cs
@@ -15,65 +15,80 @@ namespace LetterDuel.Backend.Controllers
         //Temporär lagring av spel i minnet ersätts senare med en databas
         private static List<Game> _games = new();
 
+        //Dependency injection av GameService som hanterar spelets logik och regler
         public GameController(GameService gameService)
         {
             _gameService = gameService;
         }
 
+        //Skapa nytt spel
         [HttpPost]
         public ActionResult<Game> CreateGame(CreateGameRequest request)
         {
+            //skapar första spelaren (creator)
             var player = new Player { Name = request.PlayerName };
 
+            //anropar service för att skapa spelet
             var game = _gameService.CreateGame(request.SecretWord, player);
-
+            //lägger till spel i minnet
             _games.Add(game);
 
             return Ok(game);
         }
 
+        //gå med i spel (player2)
         [HttpPost("{gameId}/join")]
         public ActionResult JoinGame(Guid gameId, JoinGameRequest request)
         {
+            //hämtar spel från databas
             var game = _games.FirstOrDefault(g => g.Id == gameId);
+            
             if (game == null)
             {
                 return NotFound("Game not found");
             }
+            //skapar ny spelare (player2)
             var player = new Player { Name = request.PlayerName };
             try
             {
+                //lägger till player2 i spelet
                 _gameService.AddPlayer(game, player);
                 return Ok(game);
             }
+            //om spel tex har 2 spelare redan
             catch (InvalidOperationException ex)
             {
                 return BadRequest(ex.Message);
             }
         }
 
+        //gissning av bokstav
         [HttpPost("{gameId}/guess")] 
         public ActionResult Guess(Guid gameId, GuessRequest request)
         {
+            //hämtar spel
             var game = _games.FirstOrDefault(g => g.Id == gameId);
 
 
             if (game == null)
                 return NotFound();
-
+            //spelservice hanterar gissning av bokstav och uppdaterar spelets state
             _gameService.GuessLetter(game, request.PlayerId, request.Letter);
 
             return Ok(game);
         }
 
+        //hämtar spelstatus
         [HttpGet("{gameId}")] 
         public ActionResult<Game> GetGame(Guid gameId)
         {
+            //hämtar spel
             var game = _games.FirstOrDefault(g => g.Id == gameId);
 
             if (game == null)
                 return NotFound();
 
+            //returnerar spelet med aktuell status
             return Ok(game);
         }   
     }
